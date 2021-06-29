32e038840267f5cf209f197ddd4d368b6b9f7122
Willian de Angelo <wdangelo1983@gmail.com>
2021-06-29 15:46:07 -0300

    correção do id da tabela tags que esta sem a primary key setada

--- nlwvaloriza/src/database/migrations/1624466343323-CreateTags.ts
+++ nlwvaloriza/src/database/migrations/1624466343323-CreateTags.ts
@@ -8,7 +8,8 @@ export class CreateTags1624466343323 implements MigrationInterface {
       columns: [
         {
           name: "id",
-          type: "uuid"
+          type: "uuid",
+          isPrimary: true
         },
         {
           name: "name",
